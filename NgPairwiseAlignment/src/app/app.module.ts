2bd67dc08a44fabbe59e3ca3e45fd17dbcc65dc3
thegodfather904 <anthonyscialo@ltgrlitmo732401.csxt.ad.csx.com>
2017-12-02 16:03:37 -0500

    added primeng and font-awesome to project

--- NgPairwiseAlignment/src/app/app.module.ts
+++ NgPairwiseAlignment/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {ButtonModule} from 'primeng/primeng';
 
 
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { UserInputComponent } from './alignment/user-input/user-input.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
