56d07b0d2755d33dc971bb3f9e615224f20860b1
thegodfather904 <anthonyscialo@Anthonys-MacBook-Pro.local>
2017-12-02 15:31:56 -0500

    started user-input component

---
+++ NgPairwiseAlignment/src/app/alignment/user-input/user-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-input',
+  templateUrl: './user-input.component.html',
+  styleUrls: ['./user-input.component.css']
+})
+export class UserInputComponent implements OnInit {
+
+  sequence1: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- NgPairwiseAlignment/src/app/app.module.ts
+++ NgPairwiseAlignment/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
+import { AlignmentComponent } from './alignment/alignment.component';
+import { UserInputComponent } from './alignment/user-input/user-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AlignmentComponent,
+    UserInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
